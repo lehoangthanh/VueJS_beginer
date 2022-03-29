0b7e892f5bb0db34ef2dc12833eeb76f43bdc9af
lehoangthanh <lehoangthanhit@gmail.com>
2022-03-29 16:05:22 +0700

    fix duplicate variable

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
- import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
